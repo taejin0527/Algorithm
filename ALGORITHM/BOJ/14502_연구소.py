da69657ad3920f391362050dc5c0bc5bfa86c519
TAEJIN KIM <taejin0323@naver.com>
2020-04-23 22:40:24 +0900

    04.23.

--- ALGORITHM/BOJ/14502_연구소.py
+++ ALGORITHM/BOJ/14502_연구소.py
@@ -1,37 +1,59 @@
 from collections import deque
-from copy import deepcopy
+
 
 N, M = map(int, input().split())
 lab = [[int(n) for n in input().split()] for _ in range(N)]
-queue = deque()
-
-
-def select_walls(_x, _y, cnt):
-    if cnt == 3:
-        new_lab = deepcopy(lab)
-
-    else:
-        for i in range(_x, N):
-            for j in range(_y, M):
-                if lab[i][j] == 0:
-                    lab[i][j] = 1
-                    select_walls(i, j, cnt + 1)
-                    lab[i][j] = 0
+virus = deque()
+safe, infected = 0, N*M
 
 
 def find_virus():
+    global safe, virus
+
     for i in range(N):
         for j in range(M):
+            if lab[i][j] != 1:
+                safe += 1
             if lab[i][j] == 2:
-                queue.append((i, j))
+                virus.append((i, j))
 
 
-def bfs():
-    while queue:
-        S = queue.popleft()
+def scenario(wall, x, y):
+    global infected
 
-        print(S)
+    if wall == 3:
+        cnt = 0
+        visited_lab = [[False]*M for _ in range(N)]
+
+        for i, j in virus:
+            cnt += dfs(i, j, visited_lab)
+
+        infected = min(infected, cnt + 3)
+        return
+
+    for nx in range(x, N):
+        k = y if nx == x else 0
+
+        for ny in range(k, M):
+            if lab[nx][ny] == 0:
+                lab[nx][ny] = 1
+                scenario(wall+1, nx, ny+1)
+                lab[nx][ny] = 0
+
+
+def dfs(x, y, visited):
+    res = 1
+    visited[x][y] = True
+    for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+        nx, ny = x+dx, y+dy
+
+        if 0 <= nx < N and 0 <= ny < M and\
+                not (visited[nx][ny] or lab[nx][ny]):
+            res += dfs(nx, ny, visited)
+
+    return res
 
 
 find_virus()
-bfs()
+scenario(0, 0, 0)
+print(safe - infected)
--- ALGORITHM/BOJ/2251_물통.py
+++ ALGORITHM/BOJ/2251_물통.py
@@ -1,28 +1,41 @@
 from collections import deque
+from itertools import permutations
+from copy import deepcopy
 
 A, B, C = map(int, input().split())
 
 
 def bfs():
-    queue = deque()
-    queue.append((0, 0, C))
-    visited = []
+    queue, visited, ans = deque(), [], []
+    limit = [A, B, C]
+    P = list(permutations([0, 1, 2], 2))
 
+    queue.append([0, 0, C])
+    visited.append([0, 0, C])
     while queue:
-        a, b, c = queue.popleft()
+        buckets = queue.popleft()
 
-        if c not in visited:
-            visited.append(c)
-            if A < C:
-                queue.append((A, 0, c - A))
+        if buckets[0] == 0:
+            ans.append(buckets[2])
+
+        for i in range(len(P)):
+            pour_from, pour_to = P[i][0], P[i][1]
+            temp = deepcopy(buckets)
+
+            if buckets[pour_from] + buckets[pour_to] > limit[pour_to]:
+                temp[pour_from] = buckets[pour_from] + buckets[pour_to] - limit[pour_to]
+                temp[pour_to] = limit[pour_to]
             else:
-                queue.append((c, 0, 0))
+                temp[pour_from] = 0
+                temp[pour_to] = buckets[pour_from] + buckets[pour_to]
 
-            if B < C:
-                queue.append((0, B, c - B))
-            else:
-                queue.append((0, c, 0))
-        print(queue)
+            if temp not in visited:
+                visited.append(temp)
+                queue.append(temp)
+
+    return ans
 
 
-bfs()
+result = bfs()
+
+print(*sorted(result))
